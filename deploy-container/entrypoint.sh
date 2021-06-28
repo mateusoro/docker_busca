716f8c001f7a704603b095df20547d05bef6ea48
mateusoro <mateusoro@gmail.com>
2021-06-28 20:19:14 -0300

    Update entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -5,7 +5,7 @@ START_DIR=/home/coder/busca
 cd ~/busca
 #node backup_automatico.js&
 pip3 install requests_html flask imdbpy bson pymongo dnspython imdbparser guessit flask_socketio mysql-connector-python
-echo $PORT
+#echo $PORT
 rclone sync rclone:/docker_buscar/busca/buscar_mysql.py /home/coder/busca/
 python3 buscar_mysql.py
 
